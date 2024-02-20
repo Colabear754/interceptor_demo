b6161a3395e1da508a7b361e4427583259f7b1c6
Colabear754 <jky2406@gmail.com>
2024-02-20 16:12:40 +0900

    🎉Initial commit

---
+++ src/main/kotlin/com/colabear754/interceptor_demo/InterceptorDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.colabear754.interceptor_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterceptorDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<InterceptorDemoApplication>(*args)
+}
